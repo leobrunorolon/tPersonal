116007938a333a140439ff7940041948ad5058af
leonardo Rolon <leobrunorolon@gmail.com>
2021-10-05 01:16:43 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// Ingreso de usuario y contraseña. usuario tiene que tener un length de 7 y includes "u". Contraseña debera tener al menos una letra mayuscula y tiene que ser al menos de 8 caracteres.
+// funciones
+function user(elDato){
+    if(elDato.length == 7 && elDato.includes("u")) {
+        return true;
+
+    } return false;
+}
+// todavia no se como hacer que verifique si por lo menos tiene una mayuscula.
+function clave(password){
+    if(password.length >=8){
+        return true;
+    } return false;
+}
+// variables
+
+let usuario = prompt("Ingresa tu usuario de lan");
+let pass = prompt("Ingresa tu contraseña.")
+// validacion
+
+if(user(usuario)){
+    console.log("Usuario correcto")
+}console.error
+
+if(clave(pass)){
+    console.log("Contraseña exitosa")
+}console.error
